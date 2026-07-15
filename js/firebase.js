04bf2f5a89e70813287e51482f1f0adc4f3ed22a
shao-projects <furgosmith21@gmail.com>
2026-07-15 20:09:26 +0800

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -130,17 +130,18 @@ document.getElementById("startBtn").addEventListener("click", () => {
 
     console.log("START COMMAND SENT");
 
-});   // <-- ito ang kulang
+});  
 
 // ===============================
 // OUTPUT STATUS
 // ===============================
-
 const outputRef = ref(db, "ehrms/outputs");
 
 onValue(outputRef, (snapshot) => {
 
     const out = snapshot.val();
+
+    console.log("OUTPUT DATA:", out);
 
     if (!out) return;
 
